b4a684d66e595244e06b1347846c9dbb83ef0ece
Samuel De Guzman <samuel.macdg@gmail.com>
2022-02-09 08:50:57 +0800

    Optimized for Heroku

--- server.js
+++ server.js
@@ -383,6 +383,6 @@ app.get('/attendances', (req, res) => {
 });
 
 // tell the server what port to listen on
-app.listen(3000, () => {
-    console.log('Listening on localhost:3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Listening...');
 });
